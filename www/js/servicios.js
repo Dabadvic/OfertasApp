ba9e6a427c0498ab32d841768dcc783966842bd1
David Abad Vich <davidabad10@gmail.com>
2015-06-17 13:22:36 +0200

    Arreglada ortografía

--- www/js/servicios.js
+++ www/js/servicios.js
@@ -4,7 +4,7 @@ angular.module('serviciosApp', [])
   var ofertas = [
     {nombre:"Bar Pepe", descripcion:"2x1 en cerveza", fin:"Hasta las 22:00", distancia:223},
     {nombre:"Cafetería 4Esquinas", descripcion:"Cafe gratis 20 clientes", fin:"Usos restantes: 13", distancia:358},
-    {nombre:"Taverna de Moe", descripcion:"Raciones 3x2", fin:"Hasta las 21:00", distancia:412}
+    {nombre:"Taberna de Moe", descripcion:"Raciones 3x2", fin:"Hasta las 21:00", distancia:412}
   ]
 
   return {
