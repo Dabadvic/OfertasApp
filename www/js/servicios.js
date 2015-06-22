2fdc1d2d53b4183a77552629578da2905d9340b4
David Abad Vich <davidabad10@gmail.com>
2015-06-22 10:14:17 +0200

    Las ofertas se cogen de la nube de datos en Parse

--- www/js/servicios.js
+++ www/js/servicios.js
@@ -1,11 +1,35 @@
 angular.module('serviciosApp', [])
 
 .factory('servicioOfertas', function() {
+  /*
   var ofertas = [
     {nombre:"Bar Pepe", descripcion:"2x1 en cerveza", fin:"Hasta las 22:00", distancia:223},
     {nombre:"Cafetería 4Esquinas", descripcion:"Cafe gratis 20 clientes", fin:"Usos restantes: 13", distancia:358},
     {nombre:"Taberna de Moe", descripcion:"Raciones 3x2", fin:"Hasta las 21:00", distancia:412}
   ]
+  */
+
+  var OfertaObject = Parse.Object.extend("OfertaObject");
+  var query = new Parse.Query(OfertaObject);
+  var ofertas = [];
+
+  query.find({
+    success: function(results) {
+      // Por cada elemento devuelto, se guarda en ofertas
+      for(var i=0; i < results.length; i++) {
+        ofertas.push({
+          nombre: results[i].get("local"),
+          descripcion: results[i].get("descripcion_corta"),
+          fin: results[i].get("fin"),
+          distancia: 250
+        });
+      }
+    },
+
+    error: function(error) {
+      console.log("Error: " + error.code + " " + error.message);
+    }
+  });
 
   return {
     ofertas: ofertas,
